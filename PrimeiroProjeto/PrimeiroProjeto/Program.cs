c68b511fc865e13b2a3b59ff365d5f01ed3b8b8b
Milton Lima <milton.mlima@gmail.com>
2025-09-02 11:19:02 -0300

    testing minimum and maximum value of int (testando valor minímo e máximo de int)

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -4,9 +4,13 @@ namespace PrimeiroProjeto {
     internal class Program {
         static void Main(string[] args) {
 
-            int n1 = int.MaxValue;
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
 
             Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
 
 
         }
