97ec7247ef7ea003a64e9cdd1f65df30e4cb09ab
Milton Lima <milton.mlima@gmail.com>
2025-09-03 16:58:56 -0300

    classes, objects and attributes2 (classes, objetos e atributos2)

---
+++ Course/Course/Funcionario.cs
@@ -0,0 +1,6 @@
+﻿namespace Course {
+    class Funcionario {
+        public string Nome;
+        public double Salario;
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,25 +1,22 @@
 ﻿using System;
+using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Pessoa p1 = new Pessoa();
-            Pessoa p2 = new Pessoa();
-            Console.WriteLine("Dados da primeira pessoa:");
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+            Console.WriteLine("Dados do primeiro funcionário:");
             Console.Write("Nome: ");
-            p1.Nome = Console.ReadLine();
-            Console.Write("Idade: ");
-            p1.Idade = int.Parse(Console.ReadLine());
-            Console.WriteLine("Dados da segunda pessoa:");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados do segundo funcionário:");
             Console.Write("Nome: ");
-            p2.Nome = Console.ReadLine();
-            Console.Write("Idade: ");
-            p2.Idade = int.Parse(Console.ReadLine());
-            if (p1.Idade > p2.Idade) {
-                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
-            }
-            else {
-                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
-            }
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double media = (f1.Salario + f2.Salario) / 2.0;
+            Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
