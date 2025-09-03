6a4329282fbddea85ff57e2da8edf885b1f397a7
Milton Lima <milton.mlima@gmail.com>
2025-09-03 11:10:21 -0300

    solving a problem without object orientation (resolvendo um problema sem orientação a objetos)

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,27 +1,9 @@
 ﻿using System;
 
 namespace Course {
-    internal class Program {
+    class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Digite três números:");
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            int n3 = int.Parse(Console.ReadLine());
-
-            double resultado = Maior(n1, n2, n3);
-
-            Console.WriteLine("O maior número é: " + resultado);
-        }
-
-        static int Maior(int a, int b, int c) {
-            int m = a;
-            if (b > m) {
-                m = b;
-            }
-            if (c > m) {
-                m = c;
-            }
-            return m; 
+            Console.WriteLine("Hello World!");
         }
     }
 }
