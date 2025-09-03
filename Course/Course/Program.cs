d5d4676dbd116210a0c12102ce9d3cc27384919b
Milton Lima <milton.mlima@gmail.com>
2025-09-03 10:45:49 -0300

    functions (funcoes)

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 
-namespace MyApp {
+namespace Course {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite três números:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3) {
+                Console.WriteLine("Maior = " + n1);
+
+            }
+            else if (n2 > n3) {
+                Console.WriteLine("Maior =" + n2);
+
+            }
+            else {
+                Console.WriteLine("Maior = " + n3);
+            }
         }
     }
 }
