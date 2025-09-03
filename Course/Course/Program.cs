dd7daf1dc5e52791316688f4eb67a9095e548e2f
Milton Lima <milton.mlima@gmail.com>
2025-09-03 11:20:10 -0300

    Make a program to read the measures of the sides of two triangles X and Y (Fazer um programa para ler as medidas dos lados de dois triângulos X e Y)

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
         }
     }
 }
