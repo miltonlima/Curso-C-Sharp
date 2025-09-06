7535460cd81c17a54c1d7d301792918275e96ad0
Milton Lima <milton.mlima@gmail.com>
2025-09-06 20:01:16 -0300

    Teste de checkout

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -5,6 +5,8 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
+
+
             double xA, xB, xC, yA, yB, yC;
 
             Console.WriteLine("Entre com as medidas do triângulo X:");
