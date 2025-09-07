6248e84c9407c6140b6729b4dc348924168f0d90
Milton Lima <milton.mlima@gmail.com>
2025-09-06 22:49:57 -0300

    projeto início

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,23 +1,9 @@
 ﻿using System;
-using System.Globalization;
+
 namespace Course {
-    class Program {
+    internal class Program {
         static void Main(string[] args) {
-
-            double xA, xB, xC, yA, yB, yC;
-
-            Console.WriteLine("Entre com as medidas do triângulo X:");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Entre com as medidas do triângulo Y:");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+            Console.WriteLine("Hello World!");
         }
     }
 }
