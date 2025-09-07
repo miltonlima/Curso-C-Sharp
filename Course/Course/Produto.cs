de42d62ddab157ab9400bf95fab67ad9bcfe24b7
Milton Lima <milton.mlima@gmail.com>
2025-09-07 00:17:33 -0300

    builder's improvement proposal (proposta de melhoria do construtor)

--- Course/Course/Produto.cs
+++ Course/Course/Produto.cs
@@ -4,6 +4,11 @@ namespace Course {
         public string Nome;
         public double Preco;
         public int Quantidade;
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
         public double ValorTotalEmEstoque() {
             return Preco * Quantidade;
         }
